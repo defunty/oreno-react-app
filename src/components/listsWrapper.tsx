cbcb8ea005f0ac4af9cbb0b2bdb9946eee2f29d2
defunty <tkqrt4131@gmail.com>
2021-05-05 23:22:01 +0900

    [Add] react-beatiful-dndへの置き換え(不必要な記述の削除)

--- src/components/listsWrapper.tsx
+++ src/components/listsWrapper.tsx
@@ -132,16 +132,13 @@ function ListWrapper() {
     init();
   }, [])
 
-
   const reorder = (listCards: CardsType, listId: number, startIndex: number, endIndex: number) => {
-    console.log('reorder');
     const result = listCards;
     const [removed] = result[listId].splice(startIndex, 1);
     result[listId].splice(endIndex, 0, removed);
     return result;
   };
   const move = (listCards: CardsType, source: any, destination: any) => {
-    console.log('move');
     const sourceClone = Array.from(listCards[parseInt(source.droppableId)]);
     const destClone = Array.from(listCards[parseInt(destination.droppableId)]);
     const [removed] = sourceClone.splice(source.index, 1);
@@ -158,44 +155,17 @@ function ListWrapper() {
       return;
     }
     if (source.droppableId === destination.droppableId) {
-      console.log(source.droppableId);
-      console.log(destination.droppableId);
-      console.log(dataState.listCards[parseInt(source.droppableId)]);
       const listCards = reorder(
         dataState.listCards,
         parseInt(source.droppableId),
         parseInt(source.index),
         parseInt(destination.index)
       );
-      // この処理は必要？（必要なさそう）
-      //if (source.droppableId === 'droppable2') {
-      //  state = { selected: items };
-      //}
       dispatch({type: 'reorderListCards', payload: listCards})
     } else {
-      console.log('debgu');
-      const listCards = move(
-        //this.getList(source.droppableId),
-        //this.getList(destination.droppableId),
-        dataState.listCards,
-        source,
-        destination
-      );
+      const listCards = move(dataState.listCards, source, destination);
       dispatch({type: 'reorderListCards', payload: listCards})
-      //console.log(result);
-      
-      //this.setState({
-      //  items: result.droppable,
-      //  selected: result.droppable2
-      //});
     }
-
-    //console.log('drop');
-    //const listsClone = Array.from(dataState.lists);
-    //const [reorderedLists] = listsClone.splice(result.source.index, 1);
-    //
-    //listsClone.splice(result.destination.index, 0, reorderedLists);
-    //console.log(listsClone)
   }
 
   const listsJSX = () => {
